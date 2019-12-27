b0ce26e77598a1391622a5689b8e13f5e7d9c859
arvind jadhav <arvindjadhav400@gmail.com>
2019-12-27 23:26:16 +0530

    test

--- index.js
+++ index.js
@@ -8,19 +8,11 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 
-app.get('/', function(request, response) {
-  var env = process.env.APP_ENV;
-  if (env == 'staging') {
-    var envName = 'staging'
-  } else if (env == 'production') {
-    var envName = 'production'
-  } else {
-    var envName = 'review app'
-  }
-  response.render('index.html', { env: envName});
+app.get('/', function (request, response) {
+  response.sendFile('index.html', { root: __dirname })
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app running at localhost:" + app.get('port'));
 });
 
